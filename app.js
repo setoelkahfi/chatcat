bb84ad671586f12c6e08c601238dbc9f91a182a5
Seto El Kahfi <setoelkahfi@gmail.com>
2015-11-05 15:35:52 +0700

    Minimal server running on port 3000

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 var express = require('express'),
 	app = express();
+	
+app.route('/').get(function(req, res, next) {
+	res.send('<h1>Hello World!</h1>');
+});
+
+app.listen(300, function() {
+	console.log('Chatcat running on port 3000');
+});
