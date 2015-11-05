3f6bf6aced7d35e9fa817b89302e0389cb79ecd5
Seto El Kahfi <setoelkahfi@gmail.com>
2015-11-05 18:22:38 +0700

    Add connect-mongo

--- app.js
+++ app.js
@@ -2,17 +2,33 @@ var express = require('express'),
 	app = express(),
 	path = require('path'),
 	cookieParser = require('cookie-parser'),
-	session = require('express-session');
+	session = require('express-session'),
+	config = require('./config/config.js'),
+	ConnectMongo = require('connect-mongo')(session);
 	
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('hogan-express'));
 app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
-app.use(session({secret:'catscanfly', saveUninitialized:true, resave:true}));
+var env = process.env.NODE_ENV || 'development';
+if (env === 'development') {
+	app.use(session({secret:config.sessionSecret, saveUninitialized:true, resave:true}));
+} else {
+	app.use(session({
+		secret:config.sessionSecret, 
+		saveUninitialized:true, 
+		resave:true,
+		store: new ConnectMongo({
+			url:config.dbURL,
+			stringify:true
+		})
+	}));
+}
 	
 require('./routes/routes.js')(express, app);
 
 app.listen(3000, function() {
 	console.log('Chatcat running on port 3000');
+	console.log('Mode: ' + env);
 });
