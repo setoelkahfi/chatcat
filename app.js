9d766916e392ecb5d3d29b7b4db5d12ccd2f9792
Seto El Kahfi <setoelkahfi@gmail.com>
2015-11-05 16:35:13 +0700

    Add hogan template engine

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 var express = require('express'),
-	app = express();
+	app = express(),
+	path = require('path');
+	
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('hogan-express'));
+app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname, 'public')));
 	
 app.route('/').get(function(req, res, next) {
-	res.send('<h1>Hello World!</h1>');
+	//res.send('<h1>Hello World!</h1>');
+	res.render('index', {});
 });
 
-app.listen(300, function() {
+app.listen(3000, function() {
 	console.log('Chatcat running on port 3000');
 });
