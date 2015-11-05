b94bc148e2caa538770fcb6180ada73123d2f309
Seto El Kahfi <setoelkahfi@gmail.com>
2015-11-05 16:45:36 +0700

    Restructured the routes module

--- app.js
+++ app.js
@@ -7,10 +7,7 @@ app.engine('html', require('hogan-express'));
 app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'public')));
 	
-app.route('/').get(function(req, res, next) {
-	//res.send('<h1>Hello World!</h1>');
-	res.render('index', {});
-});
+require('./routes/routes.js')(express, app);
 
 app.listen(3000, function() {
 	console.log('Chatcat running on port 3000');
