a2060c6aaa4e23f51f303f2730a2854fa368c4ca
Seto El Kahfi <setoelkahfi@gmail.com>
2015-11-05 17:00:56 +0700

    Add express-session

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 var express = require('express'),
 	app = express(),
 	path = require('path'),
-	cookieParser = require('cookie-parser');
+	cookieParser = require('cookie-parser'),
+	session = require('express-session');
 	
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('hogan-express'));
 app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
+app.use(session({secret:'catscanfly'}));
 	
 require('./routes/routes.js')(express, app);
 
