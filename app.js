b07eebe16512cf8592fdad93ff75e1bcb1ff8e64
Seto El Kahfi <setoelkahfi@gmail.com>
2015-11-05 16:58:00 +0700

    Add cookieParser()

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require('express'),
 	app = express(),
-	path = require('path');
+	path = require('path'),
+	cookieParser = require('cookie-parser');
 	
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('hogan-express'));
 app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
 	
 require('./routes/routes.js')(express, app);
 
