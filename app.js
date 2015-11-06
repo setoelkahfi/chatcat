8f07ed2cc3bcdf8ab0c349765aa66df0986f5025
Seto El Kahfi <setoelkahfi@gmail.com>
2015-11-06 16:13:04 +0700

    Add mongoose

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var express = require('express'),
 	session = require('express-session'),
 	config = require('./config/config.js'),
 	ConnectMongo = require('connect-mongo')(session);
+	mongoose = require('mongoose').connect(config.dbURL);
 	
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('hogan-express'));
@@ -13,18 +14,39 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
 var env = process.env.NODE_ENV || 'development';
 if (env === 'development') {
+	// development specific settings
 	app.use(session({secret:config.sessionSecret, saveUninitialized:true, resave:true}));
 } else {
+	// production specific settings
 	app.use(session({
 		secret:config.sessionSecret, 
 		saveUninitialized:true, 
 		resave:true,
 		store: new ConnectMongo({
-			url:config.dbURL,
+			//url:config.dbURL,
+			mongoose_connection:mongoose.connection[0],
 			stringify:true
 		})
 	}));
 }
+
+var userSchema = mongoose.Schema({
+	username:String,
+	password:String,
+	fullname:String
+});
+
+var Person = mongoose.model('users',userSchema);
+
+var John = new Person({
+	username: 'johndoe',
+	password:'password',
+	fullname: 'John Doe'
+});
+
+John.save(function(err) {
+	console.log('Done!');
+});
 	
 require('./routes/routes.js')(express, app);
 
