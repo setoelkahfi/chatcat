24c8bc527ac6deaae35cd5e65b24402101daee4f
Seto El Kahfi <setoelkahfi@gmail.com>
2015-11-10 18:11:51 +0700

    Add joining room functionality

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(passport.session());
 
 require('./auth/passportAuth.js')(passport, FacebookStrategy, config, mongoose);
 	
-require('./routes/routes.js')(express, app, passport, config);
+require('./routes/routes.js')(express, app, passport, config, rooms);
 
 //app.listen(3000, function() {
 //	console.log('Chatcat running on port 3000');
--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,4 @@
-module.exports = function(express, app, passport, config) {
+module.exports = function(express, app, passport, config, rooms) {
 	var router = express.Router();
 	
 	router.get('/', function(req, res, next) {
@@ -22,6 +22,23 @@ module.exports = function(express, app, passport, config) {
 	router.get('/chatrooms', securePages, function(req, res, next) {
 		res.render('chatrooms', {title:'Chatrooms', user:req.user, config:config});
 	});
+	
+	router.get('/room/:id', securePages, function(req, res, next){
+		var room_name = findTitle(req.params.id);
+		res.render('room', {user:req.user, room_number:req.params.id, room_name:room_name, config:config});
+	});
+	
+	function findTitle(room_id) {
+		var n = 0;
+		while(n<rooms.length) {
+			if(rooms[n].room_number==room_id) {
+				return rooms[n].room_name;
+				break;
+			}
+			n++;
+			continue;
+		}
+	}
 	
 	router.get('/logout', function(req, res, next){
 		req.logout();
