af2e3ebc203ac9af9049d3a48b7e97b349b8acef
Seto El Kahfi <setoelkahfi@gmail.com>
2015-11-10 17:56:06 +0700

    Add chatroom functionality

--- socket/socket.js
+++ socket/socket.js
@@ -1,5 +1,12 @@
 module.exports = function(io, rooms){
 	var chatrooms = io.of('/roomlist').on('connection', function(socket){
 		console.log('Connection established on the server');
+		socket.emit('roomupdate', JSON.stringify(rooms));
+		
+		socket.on('newRoom', function(data){
+			rooms.push(data);
+			socket.broadcast.emit('roomupdate', JSON.stringify(rooms));			
+			socket.emit('roomupdate', JSON.stringify(rooms));
+		});
 	});
 }
