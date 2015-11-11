ae3c837b322e63b81aa3d63bb9ec7e1d121852eb
Seto El Kahfi <setoelkahfi@gmail.com>
2015-11-11 13:01:54 +0700

    Chat functionality is working, ayeay'

--- socket/socket.js
+++ socket/socket.js
@@ -9,4 +9,17 @@ module.exports = function(io, rooms){
 			socket.emit('roomupdate', JSON.stringify(rooms));
 		});
 	});
+	
+	var messages = io.of('/messages').on('connection', function(socket) {
+		console.log('Connected to the chatroom!');
+		socket.on('joinroom', function(data) {
+			socket.username = data.user;
+			socket.userPic = data.userPic;
+			socket.join(data.room);
+		});
+		
+		socket.on('newMessage', function(data) {
+			socket.broadcast.to(data.room_number).emit('messagefeed', JSON.stringify(data));
+		});
+	});
 }
